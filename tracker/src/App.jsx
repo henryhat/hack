9bf30a67f255ab7c0abc874b2cfdc9da3e11924a
Henryhat <doestechhenry@gmail.com>
2026-05-26 13:38:07 +0100

    first commit

---
+++ tracker/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Transaction from "./components/Transactions"
+
+const App = () => {
+  return (
+    <div>
+      <Transaction />
+    </div>
+  )
+}
+
+export default App
